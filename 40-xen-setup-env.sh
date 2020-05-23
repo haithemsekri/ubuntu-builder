e1031302fd0c7ee6c9eff1527757e39cff58d0c9
haithemsekri <haithemsekri@hotmail.fr>
2020-05-23 17:54:26 +0200

    minimize rootfs

--- 40-xen-setup-env.sh
+++ 40-xen-setup-env.sh
@@ -6,3 +6,22 @@
 [ -z $XEN_IMAGE_TAR_FILE ]  && XEN_IMAGE_TAR_FILE="$(pwd)/cache/xen-overlays-4.11.4.tar.gz"
 [ -z $XEN_TAR_DIR_NAME ]  && XEN_TAR_DIR_NAME="xen-4.11.4"
 
+XEN_CHROOT_SCRIPT=$(cat <<EOF
+#/lib/systemd/systemd-sysv-install enable xendomains
+#/lib/systemd/systemd-sysv-install enable xendriverdomain
+#/lib/systemd/systemd-sysv-install enable xen-watchdog
+#/lib/systemd/systemd-sysv-install enable xendomains
+
+mkdir -p /etc/systemd/system/multi-user.target.wants
+cd /etc/systemd/system/multi-user.target.wants/
+ln -s /lib/systemd/system/xenconsoled.service
+ln -s /lib/systemd/system/xendriverdomain.service
+ln -s /lib/systemd/system/xen-watchdog.service
+ln -s /lib/systemd/system/xendomains.service
+ln -s /lib/systemd/system/xen-qemu-dom0-disk-backend.service
+ln -s /lib/systemd/system/xen-init-dom0.service
+ln -s /lib/systemd/system/xenstored.service
+cd -
+EOF
+)
+
